824515588aa047fcd18b525ed8f790988cf43fd5
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2022-10-30 23:23:41 +0100

    Move tests next to the code they are testing (as suggested by the Rust Book)

--- src/net.rs
+++ src/net.rs
@@ -6,9 +6,6 @@ mod net_iter;
 mod node;
 mod node_ref;
 
-#[cfg(test)]
-mod net_tests;
-
 #[derive(Default)]
 pub struct PetriNet {
     places: HashMap<PlaceRef, Place>,
@@ -193,5 +190,228 @@ impl PetriNet {
             return Err("The arc existed only in one side. The net was in an inconsistent state.");
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod net_tests {
+    use super::*;
+
+    #[test]
+    fn new_default_has_no_places() {
+        let net = PetriNet::default();
+        assert_eq!(net.get_cardinality_places(), 0);
+    }
+
+    #[test]
+    fn new_default_has_no_transitions() {
+        let new = PetriNet::default();
+        assert_eq!(new.get_cardinality_transitions(), 0)
+    }
+
+    #[test]
+    fn net_find_unconnected_places_returns_empty_set_for_empty_net() {
+        let net = PetriNet::default();
+        let unconnected_set = net.find_unconnected_places();
+
+        assert!(unconnected_set.is_empty());
+    }
+
+    #[test]
+    fn net_find_unconnected_places_some_empty_places() {
+        let mut net = PetriNet::default();
+        let place_1 = net.add_place(&"P1".to_string());
+        let place_2 = net.add_place(&"P2".to_string());
+        let place_3 = net.add_place(&"P3".to_string());
+
+        let transition_1 = net.add_transition(&"T1".to_string());
+        let transition_2 = net.add_transition(&"T2".to_string());
+
+        let result = net.add_arc_place_transition(&place_1, &transition_1);
+        assert!(result.is_ok());
+        let result = net.add_arc_transition_place(&transition_1, &place_2);
+        assert!(result.is_ok());
+        let result = net.add_arc_place_transition(&place_2, &transition_2);
+        assert!(result.is_ok());
+
+        let unconnected_set = net.find_unconnected_places();
+        assert_eq!(unconnected_set.len(), 1);
+        assert!(unconnected_set.contains(&place_3));
+    }
+
+    #[test]
+    fn net_find_unconnected_places_all_empty_places() {
+        let mut net = PetriNet::default();
+        let place_1 = net.add_place(&"P1".to_string());
+        let place_2 = net.add_place(&"P2".to_string());
+        let place_3 = net.add_place(&"P3".to_string());
+
+        let unconnected_set = net.find_unconnected_places();
+        assert_eq!(unconnected_set.len(), 3);
+        assert!(unconnected_set.contains(&place_1));
+        assert!(unconnected_set.contains(&place_2));
+        assert!(unconnected_set.contains(&place_3));
+    }
+
+    #[test]
+    fn net_add_place_updates_size() {
+        let mut net = PetriNet::default();
+        net.add_place(&"Example place".to_string());
+
+        assert_eq!(net.get_cardinality_places(), 1);
+    }
+
+    #[test]
+    fn net_add_transition_updates_size() {
+        let mut net = PetriNet::default();
+        net.add_transition(&"Example transition".to_string());
+
+        assert_eq!(net.get_cardinality_transitions(), 1);
+    }
+
+    #[test]
+    fn net_marking_returns_number_of_tokens_in_place() {
+        let mut net = PetriNet::default();
+        let place_ref = net.add_place(&"Example place".to_string());
+
+        let result = net.marking(&place_ref);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), 0);
+    }
+
+    #[test]
+    fn net_marking_add_token_increases_number_of_tokens_in_place() {
+        let mut net = PetriNet::default();
+        let place_ref = net.add_place(&"Example place".to_string());
+
+        assert!(net.add_token(&place_ref).is_ok());
+
+        let result = net.marking(&place_ref);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), 1);
+    }
+
+    #[test]
+    fn net_marking_add_token_multiple_times() {
+        let mut net = PetriNet::default();
+        let place_ref = net.add_place(&"Example place".to_string());
+
+        assert!(net.add_token(&place_ref).is_ok());
+        assert!(net.add_token(&place_ref).is_ok());
+        assert!(net.add_token(&place_ref).is_ok());
+        assert!(net.add_token(&place_ref).is_ok());
+        assert!(net.add_token(&place_ref).is_ok());
+
+        let result = net.marking(&place_ref);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), 5);
+    }
+
+    #[test]
+    fn net_marking_remove_token_decreases_number_of_tokens_in_place() {
+        let mut net = PetriNet::default();
+        let place_ref = net.add_place(&"Example place".to_string());
+
+        assert!(net.add_token(&place_ref).is_ok());
+        assert!(net.add_token(&place_ref).is_ok());
+        assert!(net.remove_token(&place_ref).is_ok());
+
+        let result = net.marking(&place_ref);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), 1);
+    }
+
+    #[test]
+    fn net_marking_remove_token_returns_err_if_place_empty() {
+        let mut net = PetriNet::default();
+        let place_ref = net.add_place(&"Example place".to_string());
+
+        assert!(net.remove_token(&place_ref).is_err());
+    }
+
+    #[test]
+    fn net_marking_vector_three_empty_places() {
+        let mut net = PetriNet::default();
+        let place_1 = net.add_place(&"P1".to_string());
+        let place_2 = net.add_place(&"P2".to_string());
+        let place_3 = net.add_place(&"P3".to_string());
+
+        let result = net.marking_vector();
+        assert_eq!(*result.get(&place_1).unwrap(), 0);
+        assert_eq!(*result.get(&place_2).unwrap(), 0);
+        assert_eq!(*result.get(&place_3).unwrap(), 0);
+    }
+
+    #[test]
+    fn net_marking_vector_two_places() {
+        let mut net = PetriNet::default();
+        let place_1 = net.add_place(&"P1".to_string());
+        let place_2 = net.add_place(&"P2".to_string());
+
+        assert!(net.add_token(&place_1).is_ok());
+        assert!(net.add_token(&place_1).is_ok());
+        assert!(net.add_token(&place_1).is_ok());
+        assert!(net.add_token(&place_1).is_ok());
+        assert!(net.add_token(&place_1).is_ok());
+
+        assert!(net.add_token(&place_2).is_ok());
+        assert!(net.add_token(&place_2).is_ok());
+        assert!(net.add_token(&place_2).is_ok());
+
+        let result = net.marking_vector();
+        assert_eq!(*result.get(&place_1).unwrap(), 5);
+        assert_eq!(*result.get(&place_2).unwrap(), 3);
+    }
+
+    #[test]
+    fn net_add_arc_place_transition_simple() {
+        let mut net = PetriNet::default();
+        let place_ref = net.add_place(&"Example place".to_string());
+        let transition_ref = net.add_transition(&"Example transition".to_string());
+
+        let result = net.add_arc_place_transition(&place_ref, &transition_ref);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn net_add_arc_transition_place_simple() {
+        let mut net = PetriNet::default();
+        let place_ref = net.add_place(&"Example place".to_string());
+        let transition_ref = net.add_transition(&"Example transition".to_string());
+
+        let result = net.add_arc_transition_place(&transition_ref, &place_ref);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn net_add_place_transition_form_self_loop() {
+        let mut net = PetriNet::default();
+        let place_ref = net.add_place(&"Example place".to_string());
+        let transition_ref = net.add_transition(&"Example transition".to_string());
+
+        let result = net.add_arc_place_transition(&place_ref, &transition_ref);
+        assert!(result.is_ok());
+        let result = net.add_arc_transition_place(&transition_ref, &place_ref);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn net_add_place_transition_form_chain() {
+        let mut net = PetriNet::default();
+        let place_1 = net.add_place(&"P1".to_string());
+        let place_2 = net.add_place(&"P2".to_string());
+        let place_3 = net.add_place(&"P3".to_string());
+
+        let transition_1 = net.add_transition(&"T1".to_string());
+        let transition_2 = net.add_transition(&"T2".to_string());
+
+        let result = net.add_arc_place_transition(&place_1, &transition_1);
+        assert!(result.is_ok());
+        let result = net.add_arc_transition_place(&transition_1, &place_2);
+        assert!(result.is_ok());
+        let result = net.add_arc_place_transition(&place_2, &transition_2);
+        assert!(result.is_ok());
+        let result = net.add_arc_transition_place(&transition_2, &place_3);
+        assert!(result.is_ok());
     }
 }
--- src/net/node.rs
+++ src/net/node.rs
@@ -3,10 +3,6 @@ use crate::net::node_ref::{PlaceRef, TransitionRef};
 use std::collections::HashSet;
 
 mod connectable;
-#[cfg(test)]
-mod place_tests;
-#[cfg(test)]
-mod transition_tests;
 
 #[derive(Default)]
 pub struct Place {
@@ -105,5 +101,191 @@ impl ConnectableNode for Transition {
 
     fn get_postset_mut(&mut self) -> &mut HashSet<Self::RefType> {
         &mut self.postset
+    }
+}
+
+#[cfg(test)]
+mod place_tests {
+    use super::*;
+
+    #[test]
+    fn place_new_assigns_label() {
+        let label = "label";
+        let place = Place::new(label.to_string());
+
+        assert_eq!(place.label, Some(label.to_string()));
+    }
+
+    #[test]
+    fn place_default_creates_place_with_no_label() {
+        let place = Place::default();
+
+        assert_eq!(place.label, None);
+    }
+
+    #[test]
+    fn place_default_is_empty() {
+        let place = Place::default();
+
+        assert!(place.is_empty());
+    }
+
+    #[test]
+    fn place_default_marking_is_zero() {
+        let place = Place::default();
+
+        assert_eq!(place.marking(), 0);
+    }
+
+    #[test]
+    fn place_add_token_updates_marking() {
+        let mut place = Place::default();
+
+        assert!(place.add_token().is_ok());
+        assert!(!place.is_empty());
+    }
+
+    #[test]
+    fn place_add_lots_of_tokens() {
+        let mut place = Place::default();
+        assert_eq!(place.marking(), 0);
+
+        for _ in 0..10 {
+            assert!(place.add_token().is_ok());
+        }
+
+        assert!(!place.is_empty());
+        assert_eq!(place.marking(), 10);
+    }
+
+    #[test]
+    fn place_remove_token_updates_length() {
+        let mut place = Place::default();
+
+        assert!(place.add_token().is_ok());
+        let result = place.remove_token();
+
+        assert!(result.is_ok());
+        assert!(place.is_empty());
+    }
+
+    #[test]
+    fn place_remove_token_returns_err_if_empty() {
+        let mut place = Place::default();
+        let result = place.remove_token();
+
+        assert!(result.is_err());
+        assert!(place.is_empty());
+    }
+
+    #[test]
+    fn place_remove_lots_of_tokens() {
+        let mut place = Place::default();
+
+        for _ in 0..10 {
+            assert!(place.add_token().is_ok());
+        }
+
+        for _ in 0..7 {
+            assert!(place.remove_token().is_ok());
+        }
+
+        assert!(!place.is_empty());
+        assert_eq!(place.marking(), 3);
+    }
+
+    #[test]
+    fn place_add_incoming_transition_returns_true_when_success() {
+        let mut place = Place::new("Example place".to_string());
+        let reference = TransitionRef("Example transition".to_string());
+
+        assert!(place.add_incoming(reference));
+    }
+
+    #[test]
+    fn place_add_incoming_transition_returns_false_when_already_exists() {
+        let mut place = Place::new("Example place".to_string());
+        let reference = TransitionRef("Example transition".to_string());
+
+        assert!(place.add_incoming(reference));
+        let reference = TransitionRef("Example transition".to_string());
+        assert!(!place.add_incoming(reference));
+    }
+
+    #[test]
+    fn place_remove_incoming_transition_returns_true_when_success() {
+        let mut place = Place::new("Example place".to_string());
+        let reference = TransitionRef("Example transition".to_string());
+
+        assert!(place.add_incoming(reference));
+        let reference = TransitionRef("Example transition".to_string());
+        assert!(place.remove_incoming(&reference));
+    }
+
+    #[test]
+    fn place_remove_incoming_transition_returns_false_when_not_found() {
+        let mut place = Place::new("Example place".to_string());
+        let reference = TransitionRef("Example transition".to_string());
+
+        assert!(place.add_incoming(reference));
+        let reference = TransitionRef("Example not found".to_string());
+        assert!(!place.remove_incoming(&reference));
+    }
+}
+
+#[cfg(test)]
+mod transition_tests {
+    use super::*;
+
+    #[test]
+    fn transition_new_assigns_label() {
+        let label = "label";
+        let transition = Transition::new(label.to_string());
+
+        assert_eq!(transition.label, Some(label.to_string()));
+    }
+
+    #[test]
+    fn transition_default_creates_transition_with_no_label() {
+        let transition = Transition::default();
+        assert_eq!(transition.label, None);
+    }
+
+    #[test]
+    fn transition_add_incoming_place_returns_true_when_success() {
+        let mut transition = Transition::new("Example transition".to_string());
+        let reference = PlaceRef("Example place".to_string());
+
+        assert!(transition.add_incoming(reference));
+    }
+
+    #[test]
+    fn transition_add_incoming_place_returns_false_when_already_exists() {
+        let mut transition = Transition::new("Example transition".to_string());
+        let reference = PlaceRef("Example place".to_string());
+
+        assert!(transition.add_incoming(reference));
+        let reference = PlaceRef("Example place".to_string());
+        assert!(!transition.add_incoming(reference));
+    }
+
+    #[test]
+    fn transition_remove_incoming_place_returns_true_when_success() {
+        let mut transition = Transition::new("Example transition".to_string());
+        let reference = PlaceRef("Example place".to_string());
+
+        assert!(transition.add_incoming(reference));
+        let reference = PlaceRef("Example place".to_string());
+        assert!(transition.remove_incoming(&reference));
+    }
+
+    #[test]
+    fn transition_remove_incoming_place_returns_false_when_not_found() {
+        let mut transition = Transition::new("Example transition".to_string());
+        let reference = PlaceRef("Example place".to_string());
+
+        assert!(transition.add_incoming(reference));
+        let reference = PlaceRef("Example not found".to_string());
+        assert!(!transition.remove_incoming(&reference));
     }
 }
