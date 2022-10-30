0840287ea4d76851e0de637d7b875f457948974c
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2022-10-30 22:57:45 +0100

    Added tests for iterators

--- src/net/net_tests.rs
+++ src/net/net_tests.rs
@@ -57,13 +57,25 @@ fn net_find_unconnected_places_all_empty_places() {
 }
 
 #[test]
+fn net_places_iter_empty_net() {
+    let net = PetriNet::default();
+    assert_eq!(net.places_iter().count(), 0);
+}
+
+#[test]
 fn net_places_iter_visits_all_places() {
     let mut net = PetriNet::default();
     net.add_place(&"P1".to_string());
     net.add_place(&"P2".to_string());
     net.add_place(&"P3".to_string());
     net.add_place(&"P4".to_string());
-    assert_eq!(net.places_iter().len(), 4);
+    assert_eq!(net.places_iter().count(), 4);
+}
+
+#[test]
+fn net_transitions_iter_empty_net() {
+    let net = PetriNet::default();
+    assert_eq!(net.transitions_iter().count(), 0);
 }
 
 #[test]
@@ -73,7 +85,7 @@ fn net_transitions_iter_visits_all_transitions() {
     net.add_transition(&"T2".to_string());
     net.add_transition(&"T3".to_string());
     net.add_transition(&"T4".to_string());
-    assert_eq!(net.transitions_iter().len(), 4);
+    assert_eq!(net.transitions_iter().count(), 4);
 }
 
 #[test]
