6c32eb694a0e832451f6900258aa2af5bcec0404
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2022-11-01 16:03:52 +0100

    Added tests for net_creator

--- src/net_creator.rs
+++ src/net_creator.rs
@@ -34,14 +34,14 @@ pub fn create_net_chain_topology(length: usize) -> PetriNet {
     }
     let mut net = create_basic_unconnected_net(length, length - 1);
 
-    for i in 1..length - 1 {
+    for i in 1..=length - 1 {
         let place_ref = PlaceRef(place_label_from_index(i));
         let transition_ref = TransitionRef(transition_label_from_index(i));
         net.add_arc_place_transition(&place_ref, &transition_ref)
             .expect("Failed while creating a net with chain topology");
     }
 
-    for i in 1..length - 2 {
+    for i in 1..=length - 1 {
         let transition_ref = TransitionRef(transition_label_from_index(i));
         let place_ref = PlaceRef(place_label_from_index(i + 1));
         net.add_arc_transition_place(&transition_ref, &place_ref)
@@ -62,4 +62,118 @@ pub fn create_net_loop_topology() -> PetriNet {
         .expect("Failed while trying to create a simple net with a loop topology");
 
     net
+}
+
+#[cfg(test)]
+mod net_creator_test {
+    use super::*;
+
+    #[test]
+    fn create_basic_unconnected_net_has_correct_number_of_nodes() {
+        let net = create_basic_unconnected_net(8, 14);
+
+        assert_eq!(net.get_cardinality_places(), 8);
+        assert_eq!(net.get_cardinality_transitions(), 14);
+    }
+
+    #[test]
+    fn create_basic_unconnected_net_has_correct_number_of_nodes_from_0_to_10() {
+        for number_of_places in 0..=10 {
+            for number_of_transitions in 0..=10 {
+                let net = create_basic_unconnected_net(number_of_places, number_of_transitions);
+                assert_eq!(net.get_cardinality_places(), number_of_places);
+                assert_eq!(net.get_cardinality_transitions(), number_of_transitions);
+            }
+        }
+    }
+
+    #[test]
+    fn create_basic_unconnected_net_has_no_arcs() {
+        let net = create_basic_unconnected_net(8, 14);
+        let arcs_1 = net.find_arcs_place_transition();
+        let arcs_2 = net.find_arcs_transition_place();
+
+        assert!(arcs_1.is_empty());
+        assert!(arcs_2.is_empty());
+    }
+
+    #[test]
+    fn create_basic_unconnected_net_has_no_arcs_from_0_to_10() {
+        for number_of_places in 0..=10 {
+            for number_of_transitions in 0..=10 {
+                let net = create_basic_unconnected_net(number_of_places, number_of_transitions);
+                let arcs_1 = net.find_arcs_place_transition();
+                let arcs_2 = net.find_arcs_transition_place();
+
+                assert!(arcs_1.is_empty());
+                assert!(arcs_2.is_empty());
+            }
+        }
+    }
+
+    #[test]
+    fn create_net_chain_topology_has_correct_number_of_nodes() {
+        let net = create_net_chain_topology(3);
+
+        assert_eq!(net.get_cardinality_places(), 3);
+        assert_eq!(net.get_cardinality_transitions(), 2);
+    }
+
+    #[test]
+    fn create_net_chain_topology_length_zero_returns_empty_net() {
+        let net = create_net_chain_topology(0);
+
+        assert_eq!(net.get_cardinality_places(), 0);
+        assert_eq!(net.get_cardinality_transitions(), 0);
+    }
+
+    #[test]
+    fn create_net_chain_topology_has_correct_number_of_nodes_from_1_to_10() {
+        for length in 1..=10 {
+            let net = create_net_chain_topology(length);
+
+            assert_eq!(net.get_cardinality_places(), length);
+            assert_eq!(net.get_cardinality_transitions(), length - 1);
+        }
+    }
+
+    #[test]
+    fn create_net_chain_topology_has_correct_number_of_arcs() {
+        let net = create_net_chain_topology(3);
+        let arcs_1 = net.find_arcs_place_transition();
+        let arcs_2 = net.find_arcs_transition_place();
+
+        assert_eq!(arcs_1.len(), 2);
+        assert_eq!(arcs_2.len(), 2);
+    }
+
+    #[test]
+    fn create_net_chain_topology_has_correct_number_of_arcs_from_1_to_10() {
+        for length in 1..=10 {
+            let net = create_net_chain_topology(length);
+            let arcs_1 = net.find_arcs_place_transition();
+            let arcs_2 = net.find_arcs_transition_place();
+
+            assert_eq!(arcs_1.len(), length - 1);
+            assert_eq!(arcs_2.len(), length - 1);
+        }
+    }
+
+    #[test]
+    fn create_net_loop_topology_has_correct_number_of_places() {
+        let net = create_net_loop_topology();
+
+        assert_eq!(net.get_cardinality_places(), 1);
+        assert_eq!(net.get_cardinality_transitions(), 1);
+    }
+
+    #[test]
+    fn create_net_loop_topology_has_correct_number_of_arcs() {
+        let net = create_net_loop_topology();
+        let arcs_1 = net.find_arcs_place_transition();
+        let arcs_2 = net.find_arcs_transition_place();
+
+        assert_eq!(arcs_1.len(), 1);
+        assert_eq!(arcs_2.len(), 1);
+    }
 }
